3507568af1f1c41372e876a6c8c8df93959a177d
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-04 16:48:41 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+//Name : Dheer Ajmera
+//Prn : 035
+//Batch : AIML A2
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StudentManagement sm = new StudentManagement();
