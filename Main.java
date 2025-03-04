9373dc358fb2a787fecc3c44fa1569b3a9f20e6e
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-04 16:51:46 +0530

    creating a menu

--- Main.java
+++ Main.java
@@ -6,3 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         StudentManagement sm = new StudentManagement();
+        
+         while (true) {
+            System.out.println("\n---- Student Management System ----");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Students");
+            System.out.println("3. Search by PRN");
+            System.out.println("4. Search by Name");
+            System.out.println("5. Search by Position");
+            System.out.println("6. Update Student Details");
+            System.out.println("7. Delete Student");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
+
