88176faedc06760413b7f9e4b5f8d764bf9764e0
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-03-04 16:38:34 +0530

    updated and initialized getter and setter for encapsulation

--- Student.java
+++ Student.java
@@ -4,3 +4,25 @@ public class Student {
     private String name;
     private Date dob;
     private double marks;
+    
+// Constructor to initialize student details
+    public Student(String prn, String name, Date dob, double marks) {
+        this.prn = prn;
+        this.name = name;
+        this.dob = dob;
+        this.marks = marks;
+    }
+
+// Getter and Setter methods for encapsulation
+    public String getPRN() { return prn; }
+    public void setPRN(String prn) { this.prn = prn; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Date getDOB() { return dob; }
+    public void setDOB(Date dob) { this.dob = dob; }
+
+    public double getMarks() { return marks; }
+    public void setMarks(double marks) { this.marks = marks; }
+    
